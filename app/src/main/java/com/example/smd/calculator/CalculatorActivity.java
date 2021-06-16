157b0a3ac29e3e95c4750816148e3b915b167583
tahakhan98 <55236046+tahakhan98@users.noreply.github.com>
2021-06-16 20:49:35 +0500

    Calculator

---
+++ app/src/main/java/com/example/smd/calculator/CalculatorActivity.java
@@ -0,0 +1,16 @@
+package com.example.smd.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+        setContentView(new CalculatorView(this,null));
+    }
+}
